bb0b9b14c7f1fd867b53dafbef954fc2924b0876
michelle nderi <nderimichelle@gmail.com>
2023-03-21 21:54:03 +0300

    Updated grade-generator

--- grade-generator/script.js
+++ grade-generator/script.js
@@ -1,6 +1,7 @@
-// function that prompts the mark as the grade and returns the grade as the output
+// function that prompts the mark as the arguement and returns the grade as the output
 
 function gradeGenerator(mark) {
+  // for any mark >100 or <0 return invalid   
   if (mark > 100 || mark < 0) {
     return "Invalid input";
   } else if (mark > 79) {
